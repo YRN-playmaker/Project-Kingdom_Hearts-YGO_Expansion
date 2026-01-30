0fd4221a329f78b747997c655403ad9310dd5a36
YRN-playmaker <2213993473a@gmail.com>
2026-01-31 03:03:03 +0800

    Add files via upload

--- script/c100025.lua
+++ script/c100025.lua
@@ -1,78 +1,82 @@
---通常魔法卡
+-- 通常魔法：指令动作-键刃变形
 function c100025.initial_effect(c)
-    -- ①效果：检索装备魔法卡
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetCountLimit(1,100025)
-    e1:SetCost(c100025.cost)
-    e1:SetTarget(c100025.target)
-    e1:SetOperation(c100025.operation)
-    c:RegisterEffect(e1)
+	-- ①效果：场上送墓 -> 卡组检索
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100025,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	-- 注意：两个效果共享同一个 CountLimit ID，实现“二选一”
+	e1:SetCountLimit(1,100025)
+	e1:SetCost(c100025.cost)
+	e1:SetTarget(c100025.target)
+	e1:SetOperation(c100025.operation)
+	c:RegisterEffect(e1)
 
-    -- ②效果：墓地效果
-    local e2=Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_GRAVE)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetCountLimit(1,100025)
-    e2:SetCost(aux.bfgcost)
-    e2:SetTarget(c100025.grave_target)
-    e2:SetOperation(c100025.grave_operation)
-    c:RegisterEffect(e2)
+	-- ②效果：墓地除外 -> 墓地回收
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100025,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	-- 注意：与 e1 使用相同的 ID
+	e2:SetCountLimit(1,100025)
+	e2:SetCost(aux.bfgcost) -- 标准的“把墓地这张卡除外”Cost
+	e2:SetTarget(c100025.grave_target)
+	e2:SetOperation(c100025.grave_operation)
+	c:RegisterEffect(e2)
 end
 
--- ①效果的cost：送1张场上的装备魔法卡去墓地
+-- ==================================================
+-- ①效果部分
+-- ==================================================
+-- Cost: 把自己场上1张装备魔法卡送去墓地
 function c100025.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_SZONE,0,1,nil,TYPE_EQUIP) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_SZONE,0,1,1,nil,TYPE_EQUIP)
-    Duel.SendtoGrave(g,REASON_COST)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_SZONE,0,1,nil,TYPE_EQUIP) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_SZONE,0,1,1,nil,TYPE_EQUIP)
+	Duel.SendtoGrave(g,REASON_COST)
 end
 
--- ①效果的target：从卡组检索1张不同名字的装备魔法卡
+-- Target: 确认卡组有装备魔法
 function c100025.filter(c)
-    return c:IsType(TYPE_EQUIP) and c:IsAbleToHand()
+	return c:IsType(TYPE_EQUIP) and c:IsAbleToHand()
 end
 function c100025.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(c100025.filter,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100025.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
--- ①效果的operation：将选中的装备魔法卡加入手牌
+-- Operation: 检索上手
 function c100025.operation(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,c100025.filter,tp,LOCATION_DECK,0,1,1,nil)
-    if g:GetCount()>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
-    end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100025.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
 
--- ②效果的target：选择墓地中的1只「键刃使」怪兽
+-- ==================================================
+-- ②效果部分
+-- ==================================================
+-- Target: 选择墓地怪兽
 function c100025.grave_filter(c)
-    return c:IsSetCard(0x208) and c:IsAbleToHand()
+	return c:IsSetCard(0x208) and c:IsAbleToHand()
 end
 function c100025.grave_target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c100025.grave_filter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(c100025.grave_filter,tp,LOCATION_GRAVE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectTarget(tp,c100025.grave_filter,tp,LOCATION_GRAVE,0,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c100025.grave_filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100025.grave_filter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c100025.grave_filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 
--- ②效果的operation：将魔法卡返回卡组，并将选中的怪兽加入手牌
+-- Operation: 回收上手
 function c100025.grave_operation(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
-        if Duel.SendtoDeck(e:GetHandler(),nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=0 then
-            Duel.SendtoHand(tc,nil,REASON_EFFECT)
-        end
-    end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
 end
-
-
--- script/c100026.lua
+++ script/c100026.lua
@@ -1,6 +1,7 @@
---王国之键
+--键刃-王国之键
 function c100026.initial_effect(c)
-	--Activate
+	--装备魔法通用注册
+	c:SetUniqueOnField(1,0,100026) -- (可选) 如果你想限制场上只能有一把王国之键，可以加这行
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -10,64 +11,61 @@ function c100026.initial_effect(c)
 	e1:SetOperation(c100026.operation)
 	c:RegisterEffect(e1)
 
-	--Atk up
+	--装备限制
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(300)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetValue(c100026.eqlimit)
 	c:RegisterEffect(e2)
-	
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+
+	--Atk up (只加攻击力)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetValue(300)
 	c:RegisterEffect(e3)
 
-	--Equip limit
+	--穿防 (Pierce)
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_EQUIP_LIMIT)
-	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetValue(c100026.eqlimit)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e4)
 
-	--to deck
+	--②效果：Cost送墓 -> 特殊召唤
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(100026,0))
-	e5:SetCategory(CATEGORY_TODECK)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e5:SetCode(EVENT_TO_GRAVE)
-	e5:SetTarget(c100026.tdtg)
-	e5:SetOperation(c100026.tdop)
+	e5:SetDescription(aux.Stringid(100026,1))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,100026)
+	e5:SetCost(c100026.spcost)
+	e5:SetTarget(c100026.sptg)
+	e5:SetOperation(c100026.spop)
 	c:RegisterEffect(e5)
 
-	--pierce
+	--③效果：送墓回卡组 (强制效果)
 	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_EQUIP)
-	e6:SetCode(EFFECT_PIERCE)
+	e6:SetDescription(aux.Stringid(100026,0))
+	e6:SetCategory(CATEGORY_TODECK)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_TO_GRAVE)
+	e6:SetTarget(c100026.tdtg)
+	e6:SetOperation(c100026.tdop)
 	c:RegisterEffect(e6)
-
-	--②效果：特殊召唤[键刃使 索拉2nd]
-	local e7=Effect.CreateEffect(c)
-	e7:SetDescription(aux.Stringid(100026,1))
-	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e7:SetType(EFFECT_TYPE_IGNITION)
-	e7:SetRange(LOCATION_SZONE)
-	e7:SetCountLimit(1,100026)
-	e7:SetCondition(c100026.spcon)
-	e7:SetCost(c100026.spcost)
-	e7:SetTarget(c100026.sptg)
-	e7:SetOperation(c100026.spop)
-	c:RegisterEffect(e7)
 end
 
+-- 装备限制：名称含有「键刃使」
 function c100026.eqlimit(e,c)
-	return c:IsSetCard(0x208) -- 限制只能装备给名称中含有「键刃使」字样的怪兽
+	return c:IsSetCard(0x208)
 end
 
+-- 装备对象选择
 function c100026.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and c100026.eqlimit(e,chkc) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,c100026.eqlimit) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,c100026.eqlimit)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
 
@@ -78,44 +76,51 @@ function c100026.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- ②效果的发动条件
-function c100026.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+-- ==================================================================
+-- ②效果：特招处理
+-- ==================================================================
+
+function c100026.costfilter(c)
+	return c:IsCode(100025) and c:IsAbleToGraveAsCost() -- [指令动作-键刃变形]
 end
 
--- ②效果的cost：从手卡送去1张[指令动作-键刃变形]和装备怪兽
+-- Cost: 把 手卡魔法 + 自身 + 装备怪兽 一起送墓
 function c100026.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local tc=c:GetEquipTarget()
-	if chk==0 then return tc and Duel.IsExistingMatchingCard(c100026.costfilter,tp,LOCATION_HAND,0,1,nil)
-		and c:IsAbleToGrave() and tc:IsAbleToGrave() end
+	if chk==0 then 
+		return tc and tc:IsAbleToGraveAsCost() 
+		and c:IsAbleToGraveAsCost()
+		and Duel.IsExistingMatchingCard(c100026.costfilter,tp,LOCATION_HAND,0,1,nil)
+	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c100026.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	g:AddCard(c)
+	g:AddCard(tc)
 	Duel.SendtoGrave(g,REASON_COST)
-	Duel.SendtoGrave(c,REASON_COST)
-	Duel.SendtoGrave(tc,REASON_COST)
-end
-
-function c100026.costfilter(c)
-	return c:IsCode(100025) and c:IsAbleToGrave() --[指令动作-键刃变形]的卡片编号
-end
-
--- ②效果的目标：特殊召唤[键刃使 索拉2nd]
-function c100026.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100026.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
 function c100026.spfilter(c,e,tp)
-	return c:IsCode(100003) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) --[键刃使 索拉2nd]的卡片编号
+	return c:IsCode(100003) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
--- ②效果的操作：特殊召唤[键刃使 索拉2nd]
+-- Target: 检查额外卡组有没有索拉2nd，并且检查空位 (SendtoGrave之后会有空位)
+function c100026.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then 
+		-- 这里使用 GetLocationCountFromEx，因为Cost会把怪兽腾出来，所以要传入e:GetHandler():GetEquipTarget()作为即将离场的怪
+		local ec=e:GetHandler():GetEquipTarget()
+		return Duel.GetLocationCountFromEx(tp,tp,ec,nil)>0 
+		and Duel.IsExistingMatchingCard(c100026.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) 
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
 function c100026.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local tc=Duel.SelectMatchingCard(tp,c100026.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp):GetFirst()
-	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		-- 限制该回合只能特殊召唤光属性怪兽
+	if Duel.GetLocationCountFromEx(tp)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100026.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		-- 成功特招后，加上光属性自肃
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
@@ -131,14 +136,17 @@ function c100026.splimit(e,c)
 	return not c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 
+-- ==================================================================
+-- ③效果：回卡组
+-- ==================================================================
 function c100026.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsRelateToEffect(e) end
+	if chk==0 then return true end -- 强制效果，chk==0 直接返回 true
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
 end
 
 function c100026.tdop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SendtoDeck(e:GetHandler(),nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoDeck(c,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 	end
 end
-
